01c26ce49b17e7453d1351b0bee650842f1b27f3
koseungbin <kosb15@naver.com>
2017-07-14 13:09:47 +0900

    환경설정

---
+++ app/src/main/java/com/landvibe/goodbeing/goodbeing_android/SurveyWriteActivity.java
@@ -0,0 +1,62 @@
+package com.landvibe.goodbeing.goodbeing_android;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Button;
+
+/**
+ * Created by 고승빈 on 2017-07-14.
+ */
+public class SurveyWriteActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surveywrite);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_intro) {
+
+        } else if (id == R.id.nav_history) {
+
+        } else if (id == R.id.nav_surveyWrite) {
+
+        } else if (id == R.id.nav_measure) {
+
+        } else if (id == R.id.nav_consulting) {
+
+        } else if (id == R.id.nav_faq) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+}
