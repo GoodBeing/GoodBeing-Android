93347881fa3ac28b85b3ae560941714e5e779707
JAEHYUNG\user <ajtnlaka456@naver.com>
2017-07-17 12:54:14 +0900

    make IntroActivity and Intro_layout

--- app/src/main/java/com/landvibe/goodbeing/goodbeing_android/IntroActivity.java
+++ app/src/main/java/com/landvibe/goodbeing/goodbeing_android/IntroActivity.java
@@ -1,30 +1,29 @@
 package com.landvibe.goodbeing.goodbeing_android;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener , View.OnClickListener{
+/**
+ * Created by user on 2017-07-17.
+ */
 
+public class IntroActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener , View.OnClickListener {
 
-    private Button mainSurveyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -46,53 +45,19 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        // 설문 조사 버튼 선언
-        mainSurveyBtn = (Button) findViewById(R.id.mainSurveyBtn);
-        mainSurveyBtn.setOnClickListener(this);
+
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+
+
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_intro) {
-
-            Intent intent = new Intent(MainActivity.this , IntroActivity.class);
-            startActivity(intent);
 
         } else if (id == R.id.nav_history) {
 
@@ -112,11 +77,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onClick(View view) {
-        if(view.getId() == R.id.mainSurveyBtn)
-        {
-            Intent intent = new Intent(MainActivity.this , SurveyWriteActivity.class);
-            startActivity(intent);
-        }
+    public void onClick(View v) {
+
     }
 }
