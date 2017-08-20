96211c661276680acc8190f152c94d7c0af189d0
jik <dltkdlwr10@gmail.com>
2017-08-20 22:39:34 +0900

    상직 - 20170820 설문결과 업데이트

--- app/src/main/java/com/landvibe/goodbeing/goodbeing_android/Survey/SurveyResultActiviy.java
+++ app/src/main/java/com/landvibe/goodbeing/goodbeing_android/Survey/SurveyResultActiviy.java
@@ -47,7 +47,14 @@ public class SurveyResultActiviy extends AppCompatActivity
 
     private TextView result_score;
     private TextView result_opinion;
-
+    private float section_depoison =0;
+    private float section_bodyhealth =0;
+    private float section_stress =0;
+    private float section_house =0;
+    private float section_job =0;
+    private float section_sleep =0;
+    private float section_skin =0;
+    private float section_main =0;
     private ArrayList<SurveyItem> arraysurvey;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,35 +87,44 @@ public class SurveyResultActiviy extends AppCompatActivity
             Log.d("score is : ",i + "번 점수는 "+arraysurvey.get(i).getScore());
             resultscore += arraysurvey.get(i).getScore();
         }
-        result_score.setText("당신의 굳빙 지수는 " + resultscore + " 입니다.");
+        resultscore = (int)(resultscore*0.7);
 
+        result_score.setText("" + resultscore);
 
+        if(resultscore>=70)
+            result_opinion.setText("당신의 굳빙 상태는 매우 위협받고 있습니다! \n 유해물질에 고노출 되고 있으니 해독이 꼭필요합니다!");
+        else if(resultscore <70 && 25<resultscore)
+            result_opinion.setText("당신의 굳빙 상태는 평범합니다! \n 하지만 어느정도 유해물질에 노출되고 있으니 해독을 권장합니다!");
+        else
+            result_opinion.setText("당신의 굳빙 상태는 매우 좋습니다! \n 현재 상태를 잘 유지하여 굳빙을 이어가세요!");
+
+        cal8section(arraysurvey);
         RadarChart radarChart = (RadarChart)findViewById(R.id.survey_result_radar_chart);
         radarChart.setRotationEnabled(false);
 
         /* 그래프 데이터 입력 */
         List<RadarEntry> entries = new ArrayList<>();
 
-        entries.add(new RadarEntry(10));
-        entries.add(new RadarEntry(20));
-        entries.add(new RadarEntry(30));
-        entries.add(new RadarEntry(40));
-        entries.add(new RadarEntry(10));
-        entries.add(new RadarEntry(20));
-        entries.add(new RadarEntry(30));
-        entries.add(new RadarEntry(40));
+        entries.add(new RadarEntry(section_stress));
+        entries.add(new RadarEntry(section_house));
+        entries.add(new RadarEntry(section_job));
+        entries.add(new RadarEntry(section_sleep));
+        entries.add(new RadarEntry(section_skin));
+        entries.add(new RadarEntry(section_depoison));
+        entries.add(new RadarEntry(section_main));
+        entries.add(new RadarEntry(section_bodyhealth));
 
         /* 그래프 라벨 입력 */
         ArrayList xVals = new ArrayList();
 
-        xVals.add("주거");
-        xVals.add("노화");
+        xVals.add("스트레스");
+        xVals.add("주거환경");
         xVals.add("직업");
         xVals.add("수면");
-        xVals.add("화장품");
-        xVals.add("해독식품");
-        xVals.add("유해식품");
-        xVals.add("신진대사");
+        xVals.add("피부노출");
+        xVals.add("해독활동");
+        xVals.add("유해물 노출");
+        xVals.add("신체건강");
 
         /* 데이터 세부 설정 */
         RadarDataSet set = new RadarDataSet(entries, null);
@@ -194,5 +210,58 @@ public class SurveyResultActiviy extends AppCompatActivity
             Intent intent = new Intent(this , SampleMainActivity.class);
             startActivity(intent);
         }
+    }
+
+    public void cal8section(ArrayList<SurveyItem> array)
+    {
+        for(int i = 0; i< array.size();i++) {
+            if (array.get(i).getCode() == 1)
+                section_bodyhealth += array.get(i).getScore();
+            else if (array.get(i).getCode() == 2)
+                section_stress += array.get(i).getScore();
+            else if (array.get(i).getCode() == 3)
+                section_house += array.get(i).getScore();
+            else if (array.get(i).getCode() == 4)
+                section_job += array.get(i).getScore();
+            else if (array.get(i).getCode() == 5)
+                section_sleep += array.get(i).getScore();
+            else if (array.get(i).getCode() == 6)
+                section_skin += array.get(i).getScore();
+            else if (array.get(i).getCode() == 7)
+                section_depoison += array.get(i).getScore();
+            else
+                section_main += array.get(i).getScore();
+        }
+
+        section_bodyhealth = 5-((section_bodyhealth*5)/16);
+        if(section_bodyhealth<=0)
+            section_bodyhealth = 0;
+
+        section_stress = 5-((section_stress*5)/14);
+        if(section_stress<=0)
+            section_stress = 0;
+
+        section_house = 5-((section_house*5)/11);
+        if(section_house<=0)
+            section_house = 0;
+
+        section_job = 5-((section_job*5)/6);
+        if(section_job<=0)
+            section_job = 0;
+
+        section_sleep = 5-((section_sleep*5)/10);
+        if(section_sleep<=0)
+            section_sleep = 0;
+
+        section_skin = 5-((section_skin*5)/13);
+        if(section_skin<=0)
+            section_skin = 0;
+
+        section_depoison = ((section_depoison*5)/27);
+        if(section_depoison<=0)
+            section_depoison = 0;
+        section_main = ((section_main*5)/75);
+        if(section_main<=0)
+            section_main = 0;
     }
 }
